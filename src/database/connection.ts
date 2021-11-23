c23e96aa2ac2638e653e08aade1cff7acfb9dd6a
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-11-23 12:10:26 +0100

    added "sql" for new module

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,5 +1,17 @@
-import { PostgreSQLClientOptions, Pool } from "../../deps.ts";
+import { PostgreSQLClientOptions, Pool, postgres } from "../../deps.ts";
 import configs from "../../configs.ts";
+
+export const sql = postgres({
+  database: configs.database.database,
+  hostname: configs.database.host,
+  port: configs.database.port,
+  user: configs.database.user,
+  password: configs.database.password,
+  max: 20,
+  types: {
+    bigint: postgres.BigInt,
+  },
+});
 
 export const pool = new Pool(
   {
