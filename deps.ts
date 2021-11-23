826e42cd777a0463579bb3f1ac84f70cdd6bb33d
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-11-23 12:09:59 +0100

    added ne psql module for better query's

--- deps.ts
+++ deps.ts
@@ -18,6 +18,9 @@ export { format } from "https://deno.land/std@0.105.0/datetime/mod.ts";
 export { prompt, Input, Checkbox } from "https://deno.land/x/cliffy@v0.19.2/prompt/mod.ts";
 export * as mammoth from "https://denopkg.com/itohatweb/mammoth@cae278d1e4deb0e9c4660e586492bddbd891880a/mod.ts";
 
+// @deno-types="https://denopkg.com/porsager/postgres@e2a8595d7aa8c3c838b83b9bca7b890c1707ad2c/types/index.d.ts"
+export { default as postgres } from "https://denopkg.com/porsager/postgres@e2a8595d7aa8c3c838b83b9bca7b890c1707ad2c/deno/lib/index.js";
+
 export { renderFile, render } from "https://deno.land/x/eta@v1.12.3/mod.ts";
 export type { CallbackFn } from "https://deno.land/x/eta@v1.12.3/file-handlers.ts";
 export type { PartialConfig } from "https://deno.land/x/eta@v1.12.3/config.ts";
