f3965dfebf0d21c63a4f0855a03f95d100ea38f3
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-11-27 11:22:17 +0100

    update group cache

--- src/database/cache/groups.ts
+++ src/database/cache/groups.ts
@@ -2,9 +2,11 @@ import db from "../database.ts";
 
 export const Groups = new Map<string, bigint>();
 
-if (Groups.size === 0) {
-  const groups = await db.select(db.groups.name, db.groups.permissions).from(db.groups);
-  for (const group of groups) {
-    Groups.set(group.name, group.permissions);
+export async function cacheGroups() {
+  if (Groups.size === 0) {
+    const groups = await db.select(db.groups.name, db.groups.permissions).from(db.groups);
+    for (const group of groups) {
+      Groups.set(group.name, group.permissions);
+    }
   }
 }
