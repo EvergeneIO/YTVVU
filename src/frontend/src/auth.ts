fcce66c965e5ac28b62a596eeccd73c0eb9b9c5f
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-12-07 15:46:59 +0100

    moved toastr options | added showPassword

--- src/frontend/src/auth.ts
+++ src/frontend/src/auth.ts
@@ -1,29 +1,21 @@
-import * as $ from 'jquery';
-import validator from './utils/validator';
+import * as $ from "jquery";
+import showPassword from "./utils/show_password";
+import validator from "./utils/validator";
 
 //document ready
 $(function () {
-    $("input#username,input#email,input#password,input#checkPassword").on("blur", validator)
-    $("input#username,input#email,input#password,input#checkPassword").on("blur", () => {
-        if ($("input#username").hasClass("is-invalid") || $("input#email").hasClass("is-invalid") || $("input#password").hasClass("is-invalid") || $("input#checkPassword").hasClass("is-invalid")) {
-            return $("button#submit").addClass("disabled")
-        }
-        $("button#submit").removeClass("disabled")
-    })
+  $("input#username,input#email,input#password,input#checkPassword").on("blur", validator);
+  $("input#username,input#email,input#password,input#checkPassword").on("blur", () => {
+    if (
+      $("input#username").hasClass("is-invalid") ||
+      $("input#email").hasClass("is-invalid") ||
+      $("input#password").hasClass("is-invalid") ||
+      $("input#checkPassword").hasClass("is-invalid")
+    ) {
+      return $("button#submit").addClass("disabled");
+    }
+    $("button#submit").removeClass("disabled");
+  });
 
-    toastr.options.closeButton = true;
-    toastr.options.debug = false;
-    toastr.options.newestOnTop = true;
-    toastr.options.progressBar = true;
-    toastr.options.positionClass = "toast-top-center";
-    toastr.options.preventDuplicates = false;
-    toastr.options.onclick = null;
-    toastr.options.showDuration = 300;
-    toastr.options.hideDuration = 1000;
-    toastr.options.timeOut = 5000;
-    toastr.options.extendedTimeOut = 1000;
-    toastr.options.showEasing = "swing";
-    toastr.options.hideEasing = "linear";
-    toastr.options.showMethod = "fadeIn";
-    toastr.options.hideMethod = "fadeOut";
+  $("svg#on,svg#off").on("click", showPassword);
 });
