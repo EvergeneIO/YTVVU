eeec6c6bbda4c7ca4a4a37ba16411de20da34c8f
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-12-19 04:14:39 +0100

    better logo color

--- src/frontend/src/utils/theme_switch.ts
+++ src/frontend/src/utils/theme_switch.ts
@@ -2,10 +2,12 @@ export default function themeSwitcher(this: JQuery<Document>) {
   if ($(this).attr("id") === "sun") {
     $(this).hide();
     $("svg#moon").show();
+    $("#logo").css("background-color", "red");
     $("body").toggleClass("theme-dark");
   } else {
     $(this).hide();
     $("svg#sun").show();
+    $("#logo").css("background-color", "rgba(0, 0, 0, 0)");
     $("body").toggleClass("theme-dark");
   }
 }
